bd9fa793ddae0c871f7debdab68b09d7a6e24318
Fatimemehmedova <fatime_0624@abv.bg>
2025-04-11 23:53:52 +0300

    and

--- CarRentingWebApp/ViewModels/Reservation/CreateReservationViewModel.cs
+++ CarRentingWebApp/ViewModels/Reservation/CreateReservationViewModel.cs
@@ -5,7 +5,7 @@ namespace CarRentingWebApp.ViewModels.Reservation
     public class CreateReservationViewModel
     {
         public int VehicleId { get; set; }
-        public string Brand { get; set; } = null!;
+         public string Brand { get; set; } = null!;
          public string Model { get; set; } = null!;
          public string? Description { get; set; }
          public string ImageUrl { get; set; } = null!;
